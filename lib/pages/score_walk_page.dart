6205ad48461e12841065b0caba158ab751c21a4b
MarianaNV10 <mariana.norenav@udea.edu.co>
2024-03-25 11:45:48 -0500

    Se termina de construir la página para calificar el paseo actual.

--- lib/pages/score_walk_page.dart
+++ lib/pages/score_walk_page.dart
@@ -20,6 +20,9 @@ class _ScoreWalkPageState extends State<ScoreWalkPage> {
   bool press4 = false;
   bool press5 = false;
 
+  final _comments = TextEditingController();
+  String _saveComments = '';
+
   Future _getImage() async{
     final _imagePicker = ImagePicker();
     _image = await _imagePicker.pickImage(source: ImageSource.gallery);
@@ -31,6 +34,134 @@ class _ScoreWalkPageState extends State<ScoreWalkPage> {
     });
   }
 
+  void _scoreWalk(String num){
+    switch (num){
+      case '1':
+        if(press1 == false && press2 == false && press3 == false && press4 == false && press5 == false){
+          press1 = true;
+        }
+        else if(press1 && press2 && press3 == false && press4 == false && press5 == false){
+          press2 = !press2;
+        }
+        else if(press1 && press2 && press3 && press4 == false && press5 == false){
+          press2 = !press2;
+          press3 = !press3;
+        }
+        else if(press1 && press2 && press3 && press4 && press5 == false){
+          press2 = !press2;
+          press3 = !press3;
+          press4 = !press4;
+        }
+        else if(press1 && press2 && press3 && press4 && press5){
+          press2 = !press2;
+          press3 = !press3;
+          press4 = !press4;
+          press5 = !press5;
+        }
+        break;
+      case '2':
+        if(press1 == false && press2 == false && press3 == false && press4 == false && press5 == false){
+          press2 = !press2;
+          press1 = !press1;
+        }
+        else if(press1 && press2 == false && press3 == false && press4 == false && press5 == false){
+          press2 = !press2;
+        }
+        else if(press1 && press2 && press3 && press4 == false && press5 == false){
+          press3 = !press3;
+        }
+        else if(press1 && press2 && press3 && press4 && press5 == false){
+          press3 = !press3;
+          press4 = !press4;
+        }
+        else if(press1 && press2 && press3 && press4 && press5){
+          press3 = !press3;
+          press4 = !press4;
+          press5 = !press5;
+        }
+        break;
+      case '3':
+        if(press1 == false && press2 == false && press3 == false && press4 == false && press5 == false){
+          press1 = !press1;
+          press2 = !press3;
+          press3 = !press3;
+        }
+        else if(press1 && press2 == false && press3 == false && press4 == false && press5 == false){
+          press2 = !press3;
+          press3 = !press3;
+        }
+        else if(press1 && press2 && press3 == false && press4 == false && press5 == false){
+          press3 = !press3;
+        }
+        else if(press1 && press2 && press3 && press4 && press5 == false){
+          press4 = !press4;
+        }
+        else if(press1 && press2 && press3 && press4 && press5){
+          press4 = !press4;
+          press5 = !press5;
+        }
+        break;
+      case '4':
+        if(press1 == false && press2 == false && press3 == false && press4 == false && press5 == false){
+          press1 = !press1;
+          press2 = !press3;
+          press3 = !press3;
+          press4 = !press4;
+        }
+        else if(press1 && press2 == false && press3 == false && press4 == false && press5 == false){
+          press2 = !press3;
+          press3 = !press3;
+          press4 = !press4;
+        }
+        else if(press1 && press2 && press3 == false && press4 == false && press5 == false){
+          press3 = !press3;
+          press4 = !press4;
+        }
+        else if(press1 && press2 && press3 && press4 == false && press5 == false){
+          press4 = !press4;
+        }
+        else if(press1 && press2 && press3 && press4 && press5){
+          press5 = !press5;
+        }
+        break;
+      case '5':
+        if(press1 == false && press2 == false && press3 == false && press4 == false && press5 == false){
+          press1 = !press1;
+          press2 = !press2;
+          press3 = !press3;
+          press4 = !press4;
+          press5 = !press5;
+        }
+        else if(press1 && press2 == false && press3 == false && press4 == false && press5 == false){
+          press2 = !press2;
+          press3 = !press3;
+          press4 = !press4;
+          press5 = !press5;
+        }
+        else if(press1 && press2 && press3 == false && press4 == false && press5 == false){
+          press3 = !press3;
+          press4 = !press4;
+          press5 = !press5;
+        }
+        else if(press1 && press2 && press3 && press4 == false && press5 == false){
+          press4 = !press4;
+          press5 = !press5;
+        }
+        else if(press1 && press2 && press3 && press4 && press5 == false){
+          press5 = !press5;
+        }
+        break;
+    }
+  }
+
+  void _onSendButtonClicked(){
+    setState(() {
+      if(_comments.text != null){
+        press5 = true;
+      }
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,148 +169,175 @@ class _ScoreWalkPageState extends State<ScoreWalkPage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                "Califique el paseo",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF8492A6),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Califique el paseo",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF8492A6),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              CircleAvatar(
-                radius: 60,
-                backgroundImage: _image == null ? const AssetImage('assets/images/userProfile.png') : Image.file(File(_image!.path)).image,
-                backgroundColor: Colors.transparent,
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              const Text(
-                "Valeria Montes",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Color(0xFF8492A6),
+                const SizedBox(
+                  height: 42.0,
                 ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/images/scoreBone.png',
-                      color: press1 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
-                      width: 37,
-                    ),
-                    onPressed: (){
-                      setState(() {
-                        press1 = !press1;
-                      });
-                    },
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: _image == null ? const AssetImage('assets/images/userProfile.png') : Image.file(File(_image!.path)).image,
+                  backgroundColor: Colors.transparent,
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  "Valeria Montes",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Color(0xFF8492A6),
                   ),
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/images/scoreBone.png',
-                      color: press2 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
-                      width: 37,
+                ),
+                const SizedBox(
+                  height: 32.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/images/scoreBone.png',
+                        color: press1 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
+                        width: 37,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          _scoreWalk('1');
+                        });
+                      },
                     ),
-                    onPressed: (){
-                      setState(() {
-                        press2 = !press2;
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/images/scoreBone.png',
-                      color: press3 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
-                      width: 37,
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/images/scoreBone.png',
+                        color: press2 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
+                        width: 37,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          _scoreWalk('2');
+                        });
+                      },
                     ),
-                    onPressed: (){
-                      setState(() {
-                        press3 = !press3;
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/images/scoreBone.png',
-                      color: press4 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
-                      width: 37,
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/images/scoreBone.png',
+                        color: press3 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
+                        width: 37,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          _scoreWalk('3');
+                        });
+                      },
                     ),
-                    onPressed: (){
-                      setState(() {
-                        press4 = !press4;
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/images/scoreBone.png',
-                      color: press5 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
-                      width: 37,
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/images/scoreBone.png',
+                        color: press4 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
+                        width: 37,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          _scoreWalk('4');
+                        });
+                      },
                     ),
-                    onPressed: (){
-                      setState(() {
-                        press5 = !press5;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              SizedBox(
-                width: 200,
-                height: 45,
-                child: ElevatedButton(
-                  onPressed: (){
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/images/scoreBone.png',
+                        color: press5 ? const Color(0xFFECE500) : const Color(0xFF8492A6),
+                        width: 37,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          _scoreWalk('5');
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 32.0,
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextField(
+                    controller: _comments,
+                    minLines: 1,
+                    maxLines: 3,
+                    textCapitalization: TextCapitalization.sentences,
+                    cursorColor: const Color(0xFF737575),
+                    decoration: const InputDecoration(
+                      hintText: 'Comentarios',
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF0DB3CA),
+                        ),
+                      ),
 
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.lightBlue,
-                  ),
-                  child: const Text(
-                    'Calificar',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              SizedBox(
-                width: 200,
-                height: 45,
-                child: ElevatedButton(
-                  onPressed: (){
+                const SizedBox(
+                  height: 76.0,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: (){
+                      _onSendButtonClicked();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightBlue,
+                    ),
+                    child: const Text(
+                      'Calificar',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12.0,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: (){
 
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.lightBlue,
-                  ),
-                  child: const Text(
-                    'Pagar',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightBlue,
+                    ),
+                    child: const Text(
+                      'Pagar',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Text(
+                  _comments.text,
+                ),
+              ],
+            ),
           ),
         ),
       ),
